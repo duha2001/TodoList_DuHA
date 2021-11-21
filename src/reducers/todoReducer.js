5e90cb0ca4512d7de6a7345e03401296887f8197
duha2001 <anhduhuynh2001@gmail.com>
2021-11-21 19:06:21 +0700

    EDIT - edit code to do list on file todoreducer

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,31 +1,75 @@
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+// const todos = JSON.parse(localStorage.getItem("todos")) || [];
+const todos = [
+  {
+    id: 1,
+    name: "Học reactjs",
+    time: "22/10/2021",
+    complete: true,
+  },
+  {
+    id: 2,
+    name: "Học fastapi",
+    time: "24/11/2021",
+    success: false,
+  },
+  {
+    id: 3,
+    name: "Code Backend",
+    time: "25/11/2021",
+    success: false,
+  },
+  {
+    id: 4,
+    name: "Code Frontend",
+    time: "26/11/2021",
+    success: false,
+  },
+  {
+    id: 5,
+    name: "Đọc sách",
+    time: "28/11/2021",
+    success: false,
+  },
+  {
+    id: 6,
+    name: "Review code",
+    time: "10/12/2021",
+    success: false,
+  },
+  {
+    id: 7,
+    name: "Clean code",
+    time: "28/12/2021",
+    success: false,
+  },
+];
 const initialState = {
-    todos: todos,
+  todos: todos,
 };
 
 const todoReducer = (state = initialState, action) => {
-    switch(action.type){
-        case "ADD_TODO":{
-            const newTodos = [...state.todos, action.payload];
-            localStorage.setItem("todos", JSON.stringify(newTodos));
-            return{todos: newTodos};
-        }
-        case "UPDATE_TODO":{
-            const filterTodos = state.todos.filter(
-                (todo) => todo.id !== action.payload.id
-            );
-            const newTodos = [...filterTodos, action.payload];
-            localStorage.setItem("todos", JSON.stringify(newTodos));
-            return{todos:newTodos};
-        }
-        case "DELETE_TODO":{
-            const newTodos = state.todos.filter((todo) => todo.id !== action.payload);
-            localStorage.setItem("todos", JSON.stringify(newTodos));
-            return{todos: newTodos};
-        }
-        default:
-            return state;
+  switch (action.type) {
+    case "ADD_TODO": {
+      const newTodos = [...state.todos, action.payload];
+      localStorage.setItem("todos", JSON.stringify(newTodos));
+      return { todos: newTodos };
     }
+    case "UPDATE_TODO": {
+      const filterTodos = state.todos.filter(
+        (todo) => todo.id !== action.payload.id
+      );
+      const newTodos = [...filterTodos, action.payload];
+      localStorage.setItem("todos", JSON.stringify(newTodos));
+      return { todos: newTodos };
+    }
+    case "DELETE_TODO": {
+      const newTodos = state.todos.filter((todo) => todo.id !== action.payload);
+      localStorage.setItem("todos", JSON.stringify(newTodos));
+      return { todos: newTodos };
+    }
+    default:
+      return state;
+  }
 };
 
 export default todoReducer;
